b82e679dd1066fa5121d910676be7aed57ff3ef4
chairit1999 <chairit.2542@gmail.com>
2024-01-14 16:22:29 +0700

    [fix] clone data and remove reset leader board

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import QuestionScreen from "./components/question";
 import questionData from "./assets/question.json";
-import { resetLeaderBoardData, storeQuestionData } from "./util/storage";
+import { storeQuestionData } from "./util/storage";
 import LeaderBoard from "./components/leader-board";
 import * as Font from "expo-font";
 
@@ -46,16 +46,16 @@ export default function App() {
     return options;
   };
 
-  const randomQuestion = () => {
-    let countQuestionData = questionData.length;
+  const randomQuestion = (questionsData: QuestionType[]) => {
+    let countQuestionData = questionsData.length;
     const numberOfRandom = 20;
     const questions: QuestionType[] = [];
     for (let i = 0; i < numberOfRandom; i++) {
       const randomIndex = Math.floor(Math.random() * countQuestionData);
-      const question = questionData[randomIndex];
+      const question = questionsData[randomIndex];
       question.options = randomOption(question.options);
       questions.push({ ...question });
-      questionData.splice(randomIndex, 1);
+      questionsData.splice(randomIndex, 1);
       countQuestionData -= 1;
     }
     return questions;
@@ -65,8 +65,9 @@ export default function App() {
     const handleAppStateChange = async (nextAppState: AppStateStatus) => {
       await Font.loadAsync(customFonts);
       if (nextAppState === "active") {
-        await storeQuestionData(randomQuestion());
-        await resetLeaderBoardData();
+        const questions = [...questionData];
+        await storeQuestionData(randomQuestion(questions));
+        // await resetLeaderBoardData();
       }
     };
     const event = AppState.addEventListener("change", handleAppStateChange);
